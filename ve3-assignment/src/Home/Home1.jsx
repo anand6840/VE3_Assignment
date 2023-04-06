a443bc8e3203204ae28e537ab94e3931a7b129cc
Anand Waghmare <103956874+anand6840@users.noreply.github.com>
2023-04-06 11:08:22 +0530

    Options Page Done

--- ve3-assignment/src/Home/Home1.jsx
+++ ve3-assignment/src/Home/Home1.jsx
@@ -1,14 +1,27 @@
 import React from 'react'
 import BgImg from './background-image-2.png';
-import women from './img-1.png';
 import './home.css'
+import { Link } from 'react-router-dom';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faArrowRightLong } from '@fortawesome/free-solid-svg-icons';
 
 
 export const Home1 = () => {
   return (
     <div className='home1-main'>
-      <img src={BgImg} alt="BgImg" />;
-      <img className='women' src={women} alt="women" />;
+      <img className='BgImg' src={BgImg} alt="BgImg" />
+      <div className='home-data'>
+      <h1>Fusce sem magna pharetra cursus</h1>
+				<p>
+					Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quia reprehenderit ullam, eveniet laborum aliquam aspernatur eius reiciendis a magni pariatur dolore eaque ducimus temporibus dolorem, quam eos assumenda, natus inventore sed labore odio quae quod repellat ipsam. Quidem.
+				</p>
+        <Link className="home-enter-link" to={'/options'}>
+					<div>
+						Enter
+						<FontAwesomeIcon icon={faArrowRightLong} />
+					</div>
+				</Link>
+      </div>
     </div>
   )
 }
--- ve3-assignment/src/Home/Home2.jsx
+++ ve3-assignment/src/Home/Home2.jsx
@@ -1,7 +1,53 @@
 import React from 'react'
+import BgImg from './background-image-2.png';
+import './home.css'
+import { Link } from 'react-router-dom';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faArrowRightLong } from '@fortawesome/free-solid-svg-icons';
 
 export const Home2 = () => {
   return (
-    <div>home2</div>
+    <div className='home1-main'>
+      <img className='BgImg' src={BgImg} alt="BgImg" />
+      <div className='home-data'>
+      <Link style={{textDecoration: 'none'}} className="Module-enter-link" to={'/module1'}>
+					<div>
+						Module One
+						
+					</div>
+				</Link>
+        <Link style={{textDecoration: 'none'}} className="Module-enter-link" to={'/module2'}>
+					<div>
+          Module Two
+					
+					</div>
+				</Link>
+        <Link style={{textDecoration: 'none'}} className="Module-enter-link" to={'/module3'}>
+					<div>
+          Module Three
+						
+					</div>
+				</Link>
+        <Link style={{textDecoration: 'none'}} className="Module-enter-link" to={'/module4'}>
+					<div>
+          Module Four
+						
+					</div>
+				</Link>
+        <Link style={{textDecoration: 'none'}} className="Module-enter-link" to={'/module5'}>
+					<div>
+          Module Five
+						
+					</div>
+				</Link>
+       
+        <Link style={{textDecoration: 'none'}} className="home-enter-link" to={'/options'}>
+					<div>
+						
+						<FontAwesomeIcon icon={faArrowRightLong} />
+					</div>
+				</Link>
+      </div>
+    </div>
   )
 }
